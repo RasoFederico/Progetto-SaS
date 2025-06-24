a8d7a3222f452118332dbdc3a78908aaba939dda
Federico Raso <federico.raso@edu.unito.it>
2025-06-24 09:59:54 +0200

    primo commit

---
+++ src/main/java/catering/businesslogic/user/UserManager.java
@@ -0,0 +1,38 @@
+package catering.businesslogic.user;
+
+import java.util.logging.Logger;
+import catering.businesslogic.UseCaseLogicException;
+import catering.util.LogManager;
+
+public class UserManager {
+    private static final Logger LOGGER = LogManager.getLogger(UserManager.class);
+    private static UserManager instance = new UserManager();
+
+    private UserManager (){
+        UserManager.getInstance().getCurrentUser().isOrganizer();   
+    }
+
+    private User currentUser;
+
+    public void fakeLogin(String username) throws UseCaseLogicException {
+        LOGGER.info("Attempting login for user: " + username);
+        this.currentUser = User.load(username);
+        if (this.currentUser == null) {
+            LOGGER.warning("Login failed: user not found - " + username);
+            throw new UseCaseLogicException("User not found");
+        }
+        LOGGER.info("User successfully logged in: " + username);
+    }
+
+    public User getCurrentUser() {
+        return this.currentUser;
+    }
+
+    public static UserManager getInstance(){
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+}
